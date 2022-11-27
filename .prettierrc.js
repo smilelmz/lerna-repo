8ba5d82a290ff31ffedcb3ab5816f52985ee366d
梁美座 <1014241125@qq.com>
2022-11-27 18:13:07 +0800

    feat: test commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,7 @@
-module.exports = require('@umijs/fabric').prettier;
+module.exports = {
+  ...require('@umijs/fabric').prettier,
+  'prettier.semi': false,
+  'prettier.tabWidth': 2,
+  'prettier.singleQuote': true,
+  'prettier.trailingComma': 'none'
+}
