7f7d25c8c5a4633dfaf4d1ef7e29dac04bafb272
梁美座 <1014241125@qq.com>
2022-11-27 18:44:18 +0800

    feat: commitizen 配置调整

--- .cz-configrc.js
+++ .cz-configrc.js
@@ -30,10 +30,39 @@ module.exports = {
     },
     {
       value: 'revert',
-      name: '⏪   revert: Revert to a commit'
+      name: '⏪   revert: 代码回滚'
     }
   ],
-  scopes: [],
+  scopes: [
+    ['components', '组件相关'],
+    ['hooks', 'hook 相关'],
+    ['utils', 'utils 相关'],
+    ['styles', '样式相关'],
+    ['deps', '项目依赖'],
+    ['auth', '对 auth 修改'],
+    ['other', '其他修改'],
+    // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
+    ['custom', '以上都不是？我要自定义']
+  ].map(([value, description]) => {
+    return {
+      value,
+      name: `${value.padEnd(30)} (${description})`
+    }
+  }),
+
+  // 是否允许自定义填写 scope，在 scope 选择的时候，会有 empty 和 custom 可以选择。
   allowCustomScopes: true,
-  allowBreakingChanges: ['feat', 'fix']
+  allowBreakingChanges: ['feat', 'fix'],
+
+  messages: {
+    type: '请选择提交类型(必填)',
+    scope: '选择一个 scope (可选)',
+    // 选择 scope: custom 时会出下面的提示
+    customScope: '请输入文件修改范围(可选)',
+    subject: '请简要描述提交(必填)',
+    body: '请输入详细描述(可选)',
+    breaking: '列出任何BREAKING CHANGES(破坏性修改)(可选)',
+    footer: '请输入要关闭的issue(可选)',
+    confirmCommit: '确认提交？'
+  }
 }
